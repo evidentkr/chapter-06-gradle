b99d3f3f9bd7e8b97ef8ca7d58789ecec09257c2
김성민 <evidentkr@naver.com>
2021-01-21 18:52:23 +0900

    Initial commit

---
+++ src/main/java/com/rubypaper/persistence/BoardRepository.java
@@ -0,0 +1,7 @@
+package com.rubypaper.persistence;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.rubypaper.domain.Board;
+
+public interface BoardRepository extends CrudRepository<Board, Long> {}
